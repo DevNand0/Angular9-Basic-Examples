1979b780ae3d1662d45f30a746785f8e304054b6
DevNand0 <fernandoo.avellaneda.m@gmail.com>
2020-05-10 02:24:59 -0500

    rutas a vistas desde app-router.module.ts con redireccion y creacion de componentes utilizando nav tab

--- router-demo/src/app/app.module.ts
+++ router-demo/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
