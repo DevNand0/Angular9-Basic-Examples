37c0325a17f927323c7c9d50af5e1ff375166a2e
DevNand0 <fernandoo.avellaneda.m@gmail.com>
2020-05-14 00:50:30 -0500

    Revert "creacion de rutas para listar los departementos, envio de parametros, validar que la pagina se refresque desde el ngOnInit y testing de child routes"
    This reverts commit 3149d19880140ac4cff88a064fe592c6608d389b.

--- router-demo/src/app/app.module.ts
+++ router-demo/src/app/app.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DepartmentDetailComponent } from './department-detail/department-detail.component';
-import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
-import { DepartmentContactComponent } from './department-contact/department-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    PageNotFoundComponent,
-    DepartmentDetailComponent,
-    DepartmentOverviewComponent,
-    DepartmentContactComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
--- router-demo/src/app/department-list/department-list.component.ts
+++ router-demo/src/app/department-list/department-list.component.ts
@@ -1,38 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
   template: `
-    <h3>
-      Department List
-    </h3>
-    <ul class="items">
-      <li *ngFor="let department of departments" (click)="onSelect(department)" >
-        <span class="badge">{{department.id}}</span>{{department.name}}
-      </li>
-    </ul>
+    <p>
+      department-list works!
+    </p>
   `,
   styles: [
   ]
 })
 export class DepartmentListComponent implements OnInit {
 
-  departments=[
-    {'id':1, 'name': 'Angular'},
-    {'id':2, 'name': 'Node'},
-    {'id':3, 'name': 'MongoDB'},
-    {'id':4, 'name': 'Ruby'},
-    {'id':5, 'name': 'Bootstrap'}
-  ]
-
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSelect(department){
-    this.router.navigate(['/departmentDetail',department.id]);
   }
 
 }
